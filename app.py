d6f4bc6f818830779736a5687e286f609b0178c2
Alex Straton <alexandrastraton1@gmail.com>
2024-10-08 10:09:57 +0100

    connected to the database and created a db file

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 from quart import Quart
+from quart_db import QuartDB
+
+#app is the variable that holds the quart application object
+app=Quart(__name__)
+##name tells quart the module name(the file that contains the code)
+QuartDB(app, url="sqlite:///database.db")#connects to db
+
+#route decorator (@) defiend a route that responds to get requests
+@app.get("/")
+#index function is called whenever a get request is made to route "/"
+async def index():
+    return {"Hello": "world"}
